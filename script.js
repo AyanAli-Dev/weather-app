cfca5ccfb7d55e12842006695db140a1f7906c51
Ayan Ali <ayanalibaba467@gmail.com>
2026-06-21 01:07:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function getWeather() {
     let city = document.getElementById("city").value;
 
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=YOUR_API_KEY&units=metric`;
+ let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8f58b5ce3a0a67ccac5ae99f4ad957c9&units=metric`;
 
     let response = await fetch(url);
     let data = await response.json();
